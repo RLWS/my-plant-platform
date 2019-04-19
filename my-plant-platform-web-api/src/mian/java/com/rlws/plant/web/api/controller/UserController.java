1a002d836d5792459cd81f68d53088b345feab51
RLWS靜 <1600510083@qq.com>
2019-04-19 15:56:08 +0800

    处理jackson-databind的安全漏洞,更换更高的版本

--- my-plant-platform-web-api/src/mian/java/com/rlws/plant/web/api/controller/UserController.java
+++ my-plant-platform-web-api/src/mian/java/com/rlws/plant/web/api/controller/UserController.java
@@ -3,6 +3,7 @@ package com.rlws.plant.web.api.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.rlws.plant.commons.dto.BaseResult;
+import com.rlws.plant.commons.utils.HttpclientUtils;
 import com.rlws.plant.commons.utils.MapperUtils;
 import com.rlws.plant.domain.User;
 import com.rlws.plant.web.api.service.UserService;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "${web.rest.url}")
 public class UserController {
 
     @Autowired
@@ -21,19 +23,26 @@ public class UserController {
 
     //注册用户提交信息判断用户邮箱是否已经注册ajax
     @RequestMapping(value = "test",method = RequestMethod.GET)
-    public String selectByPrimaryKey(String email) {
+    public BaseResult selectByPrimaryKey(String email) {
         System.out.println(email);
         User user1 = userService.selectByPrimaryKey(email);
         if (user1 != null){
-            return MapperUtils.getBeanToJson(BaseResult.success(user1));
+            return BaseResult.success(user1);
         }
-        return MapperUtils.getBeanToJson(BaseResult.fail());
+        return BaseResult.fail();
     }
 
     //默认进入的页面
     @RequestMapping(value = "",method = RequestMethod.GET)
-    public String main() {
-        return "test";
+    public BaseResult main() {
+        return BaseResult.success("API服务器启动成功");
+    }
+
+    //httpClientUtils测试
+    @RequestMapping(value = "rlws",method = RequestMethod.GET)
+    public BaseResult myTest(String email){
+//        HttpclientUtils.get()
+        return null;
     }
 
 }
--- my-plant-platform-web-ui/src/mian/java/com/rlws/plant/web/ui/controller/index.java
+++ my-plant-platform-web-ui/src/mian/java/com/rlws/plant/web/ui/controller/index.java
@@ -1,14 +1,20 @@
 package com.rlws.plant.web.ui.controller;
 
+import com.rlws.plant.commons.dto.BaseResult;
+import com.rlws.plant.commons.utils.HttpclientUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class index {
 
     @RequestMapping(value = {"","index"},method = RequestMethod.GET)
+    @ResponseBody
     public String index(){
-        return "index";
+        String email = "123@qq.com";
+        String s = HttpclientUtils.get("http://127.0.0.1:8081/api/test?email=" + email);
+        return s;
     }
 }
